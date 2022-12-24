06368d728038f05243a7646db8565165ee60d293
MOUAD FIFEL <mouad.fifel5@gmail.com>
2022-12-24 22:20:00 +0100

    completjson | steper component js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,4 +9,45 @@ async function getData() {
     } catch (error) {
       console.error(error);
     }
+  } 
+  let info_div = document.getElementById("step-1");
+  let question_div = document.getElementById("step-2");
+  let result_div = document.getElementById("step-3");
+
+
+  function info()
+  {
+    info_div.classList.add("d-block");
+    question_div.classList.add("d-none");
+    result_div.classList.add("d-none");
+
+    info_div.classList.remove("d-none");
+    question_div.classList.remove("d-block");
+    result_div.classList.remove("d-block");
+
   }
+
+  function question()
+  {
+    info_div.classList.add("d-none");
+    question_div.classList.add("d-block");
+    result_div.classList.add("d-none");
+    
+    info_div.classList.remove("d-block");
+    question_div.classList.remove("d-none");
+    result_div.classList.remove("d-block");
+    
+  }
+
+  function result()
+  {
+    info_div.classList.add("d-none");
+    question_div.classList.add("d-none");
+    result_div.classList.add("d-block");
+    
+    info_div.classList.remove("d-block");
+    question_div.classList.remove("d-block");
+    result_div.classList.remove("d-none");
+    
+  }
+  
