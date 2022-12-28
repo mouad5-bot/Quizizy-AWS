9729193da14614925a7d8a880f0e5e68f05725d0
mouad FIFEL~ <mouad.fifel5@gmail.com>
2022-12-28 17:24:38 +0100

    result almost finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 let index = 0;
 let index_progress_bar = 0;
 let data = []; 
+let score = 0;
 getData();
 
   async function getData() 
@@ -16,7 +17,8 @@ getData();
 
   function getQuestions(questions)
   {
-      data=questions.sort(()=>Math.random()-0.5 ); //sort : trier 
+      //data=questions.sort(()=>Math.random()-0.5 ); //sort : trier 
+      data=questions;
   }
 
   let info_div = document.getElementById("step-1");
@@ -37,9 +39,6 @@ getData();
 
     function question()
     {
-
-      console.log(index);
-
         
         // document.getElementById("quesH5").innerText = data[index].question;
         // document.getElementById("QCM1").innerText = data[index].choices[0];
@@ -52,7 +51,7 @@ getData();
           document.getElementById('queston-container').innerHTML = `<h5 id="quesH5"> ${data[index].question} </h5>                
         <div class="row">
           <div class="col-3">
-            <input class="mt-3"  type="radio" name="q1" id="q1c" value=""> <br>
+            <input class="mt-3"  type="radio" name="q1" id="q1c" value="0"> <br>
           </div>
           <div class="col-8">
             <label for="q1c" id="QCM1">${data[index].choices[0]}</label> <br>
@@ -60,7 +59,7 @@ getData();
         </div>
         <div class="row">
           <div class="col-3">
-            <input class=" mt-3"  type="radio" name="q1" id="q2c" value=""> <br>
+            <input class=" mt-3"  type="radio" name="q1" id="q2c" value="1"> <br>
           </div>
           <div class="col-8">
             <label for="q2c" id="QCM2">${data[index].choices[1]}</label> <br>
@@ -68,7 +67,7 @@ getData();
         </div>
         <div class="row">
           <div class="col-3">
-            <input class=" mt-3"  type="radio" name="q1" id="q3c" value=""> <br>
+            <input class=" mt-3"  type="radio" name="q1" id="q3c" value="2"> <br>
           </div>
           <div class="col-8">
             <label for="q3c" id="QCM3">${data[index].choices[2]}</label> <br>
@@ -76,57 +75,19 @@ getData();
         </div>
         <div class="row">
           <div class="col-3">
-            <input class= "mt-3"  type="radio" name="q1" id="q4c" value=""> <br>
+            <input class= "mt-3"  type="radio" name="q1" id="q4c" value="3"> <br>
           </div>
           <div class="col-8">
             <label for="q4c" id="QCM4">${data[index].choices[3]}</label> <br>
           </div>
         </div>`
         index++;
+        
         }
         else {
           result();
         }
 
-        // document.getElementById('queston-container').innerHTML = `<h5 id="quesH5"> ${data[index].question} </h5>                
-        // <div class="row">
-        //   <div class="col-3">
-        //     <input class="mt-3"  type="radio" name="q1" id="q1c" value=""> <br>
-        //   </div>
-        //   <div class="col-8">
-        //     <label for="q1c" id="QCM1">${data[index].choices[0]}</label> <br>
-        //   </div>
-        // </div>
-        // <div class="row">
-        //   <div class="col-3">
-        //     <input class=" mt-3"  type="radio" name="q1" id="q2c" value=""> <br>
-        //   </div>
-        //   <div class="col-8">
-        //     <label for="q2c" id="QCM2">${data[index].choices[1]}</label> <br>
-        //   </div>
-        // </div>
-        // <div class="row">
-        //   <div class="col-3">
-        //     <input class=" mt-3"  type="radio" name="q1" id="q3c" value=""> <br>
-        //   </div>
-        //   <div class="col-8">
-        //     <label for="q3c" id="QCM3">${data[index].choices[2]}</label> <br>
-        //   </div>
-        // </div>
-        // <div class="row">
-        //   <div class="col-3">
-        //     <input class= "mt-3"  type="radio" name="q1" id="q4c" value=""> <br>
-        //   </div>
-        //   <div class="col-8">
-        //     <label for="q4c" id="QCM4">${data[index].choices[3]}</label> <br>
-        //   </div>
-        // </div>`
-        // index++;
-
-        // if (index == 10){
-        //   result();
-        // }
-         
       
     }
     
@@ -141,38 +102,39 @@ getData();
     question_div.classList.remove("d-block");
     result_div.classList.remove("d-none");
 
+    let progress_result = document.getElementById('progress-result').innerHTML = score;
+    document.getElementById('score').style.width = `${score}%`; 
 
     let resultContent = document.getElementById("result-content");
     
     for(let i = 0; i < data.length; i++)
     {
-        resultContent.innerHTML += 
-        `
-            <div class="question">
-              <div class="row">
-                <div class="col-3 ms-3 text-start">
-                  <p> <b>  question ${i+1} :</b> </p> <br>
-                </div>
-                <div class="col-8">
-                  <label for="q1c" id="QCM1"> ${data[i].question} </label> <br>      
-                </div>
+      resultContent.innerHTML += 
+      `<div class="question">
+            <div class="row">
+              <div class="col-3 ms-3 text-start">
+                <p> <b>  question ${i+1} :</b> </p> <br>
+              </div>
+              <div class="col-8">
+                <label for="q1c" id="QCM1"> ${data[i].question} </label> <br>      
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-3 ms-3 text-start">
+                <p> <b>  correct choice :</b> </p> <br>
+              </div>
+              <div class="col-8 text-start">
+                <label for="q1c" id="QCM1">${(data[i].answer)+1}</label> <br>      
               </div>
               <div class="row">
                 <div class="col-3 ms-3 text-start">
-                  <p> <b>  correct choice :</b> </p> <br>
+                  <p> <b>  description :</b> </p> <br>
                 </div>
-                <div class="col-8 text-start">
-                  <label for="q1c" id="QCM1">${(data[i].answer)+1}</label> <br>      
+                <div class="col-8 mb-5">
+                  <label for="q1c" id="QCM1">${data[i].description} </label> <br>      
                 </div>
-                <div class="row">
-                  <div class="col-3 ms-3 text-start">
-                    <p> <b>  description :</b> </p> <br>
-                  </div>
-                  <div class="col-8 mb-5">
-                    <label for="q1c" id="QCM1">${data[i].description} </label> <br>      
-                  </div>
-            </div>
-        </div>   <hr/> ` ;
+          </div>
+      </div>   <hr/> ` ;
     }
   }
   
@@ -183,13 +145,17 @@ getData();
     let checkRadio = document.querySelector("input:checked");
 
       if(checkRadio)
-      {
-          correctQuestion();       
+      {       
+        let correctQt = data[index-1].answer;
+        let radioChecked = checkRadio.value;
+        if(correctQt == radioChecked){
+          score += 100/data.length;
+        }
           question(); 
           progressBar(); 
       }else
       {
-          alert('please select one choice');
+          `<p> please select one choice </p>`
       }
   }
 
@@ -199,6 +165,8 @@ getData();
     index_progress_bar += 100/data.length;
     document.querySelector('.progress-bar').style.width = `${index_progress_bar}%`;           
   }
+
+
 
 
 function showQuestionComponent(){
@@ -212,13 +180,6 @@ function showQuestionComponent(){
     result_div.classList.remove("d-block");
 
     question();
-}
-
-
-correctQuestion()
-{
-  let correctQt = data[i].answer;
-  console.log(correctQt);
 }
 
 
